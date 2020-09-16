53a99ca6ff84e167de1c1cd46d62278f40684778
maheshschand <r.mahesh2307@gmail.com>
2020-09-16 12:41:20 +0530

    Add student enrollment

--- courses/views.py
+++ courses/views.py
@@ -15,6 +15,7 @@ from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 from django.db.models import Count
 from .models import Subject
 from django.views.generic.detail import DetailView
+from students.forms import CourseEnrollForm
 
 class OwnerMixin(object):
     def get_queryset(self):
@@ -168,3 +169,8 @@ class CourseListView(TemplateResponseMixin, View):
 class CourseDetailView(DetailView):
     model = Course
     template_name = 'courses/course/detail.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['enroll_form'] = CourseEnrollForm(
+                                   initial={'course':self.object})
+        return context
