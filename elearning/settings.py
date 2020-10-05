5172a762afa2a61fb0a3a1e1d3662590705e5ad1
maheshschand <r.mahesh2307@gmail.com>
2020-10-05 13:18:11 +0530

    Testing app

--- elearning/settings.py
+++ elearning/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '32y(sa7py#!+zp9rl!7oz@$ku(osq_vd-4yt)o9dg10#o)9&w6'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'elearning-ju.herokuapp.com', '*']
 
